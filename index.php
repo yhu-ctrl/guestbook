5a119a744ddaf09925710bfbe4b94103c1f1d3fe
iii <cclan8@outlook.com>
2019-04-18 22:57:18 +0800

    已实现用户的登录与注销

--- index.php
+++ index.php
@@ -1,41 +1,36 @@
+<?php
+session_start();
+if (!isset($_SESSION['uid'])) {
+    # 表示当前未登录，跳转到登录页面
+    exit('<script>self.location="login.php";</script>');
+}
+if (isset($_GET['action']) && $_GET['action'] == 'logout') {
+    # 用户退出
+    $_SESSION = array();
+    exit('<script>self.location="login.php";</script>');
+}
+
+$uid = $_SESSION['uid'];
+
+require_once('config.php');
+$sql = "SELECT username, nickname FROM userlist WHERE uid = '$uid' ";
+$result = $db->query($sql);
+if ($result->num_rows === 0)
+    exit('<script>alert("该用户不存在!");history.back();</script>');
+$user = $result->fetch_assoc();
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/materialize.min.css">
-    <link rel="stylesheet" href="css/icon.css">
     <title>留言板</title>
 </head>
-
 <body>
-    <div class="container">
-        <h3>登录</h3>
-        <form action="login.php" method="POST">
-            <div class="row">
-                <div class="input-field col s12">
-                    <i class="material-icons prefix">account_circle</i>
-                    <input type="text" name="name" id="name" class="validate">
-                    <label for="name">用户名:</label>
-                </div>
-            </div>
-            <div class="row">
-                <div class="input-field col s12">
-                    <i class="material-icons prefix">lock</i>
-                    <input type="password" name="passwd" id="passwd" class="validate">
-                    <label for="passwd">密码:</label>
-                </div>
-            </div>
-            <div class="row">
-                <button type="submit" class="btn">登录</button>
-                <a href="reg.php" class="btn">注册</a>
-            </div>
-        </form>
-    </div>
-
-    <script src="js/materialize.min.js"></script>
+    <?php echo $user['nickname']; ?> 登录成功
+    <a href="index.php?action=logout">注销</a>
 </body>
-
 </html>
--- login.php
+++ login.php
@@ -1,33 +1,70 @@
 <?php
-$username = $_POST['name'];
-$password = $_POST['passwd'];
+session_start();
+if (isset($_SESSION['uid'])){
+    exit('<script>self.location="/";</script>');
+}
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $username = $_POST['name'];
+    $password = $_POST['passwd'];
 
-if(!$username)
-    exit('<script>alert("请输入用户名!");history.back();</script>');
-if(!$password)
-    exit('<script>alert("请输入密码!");history.back();</script>');
-$password = md5($password);
+    if(!$username)
+        exit('<script>alert("请输入用户名!");history.back();</script>');
+    if(!$password)
+        exit('<script>alert("请输入密码!");history.back();</script>');
+    $password = md5($password);
 
-require_once('config.php');
-$sql = "SELECT username, password, nickname
-        FROM userlist
-        WHERE username = '$username'";
-$result = $db->query($sql);
-if ($result->num_rows === 0)
-    exit('<script>alert("该用户不存在!");history.back();</script>');
-$user = $result->fetch_assoc();
-if ($user['password'] != $password)
-    exit('<script>alert("密码错误!");history.back();</script>');
+    require_once('config.php');
+    $sql = "SELECT uid, username, password, nickname FROM userlist WHERE username = '$username'";
+    $result = $db->query($sql);
+    if ($result->num_rows === 0)
+        exit('<script>alert("该用户不存在!");history.back();</script>');
+    $user = $result->fetch_assoc();
+    if ($user['password'] != $password)
+        exit('<script>alert("密码错误!");history.back();</script>');
+
+    $_SESSION['uid'] = $user['uid'];
+    exit('<script>self.location="/";</script>');
+}
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/materialize.min.css">
+    <link rel="stylesheet" href="css/icon.css">
     <title>留言板</title>
 </head>
+
 <body>
-    <?php echo $user['nickname']; ?> 登录成功
+    <div class="container">
+        <h3>登录</h3>
+        <form action="#" method="POST">
+            <div class="row">
+                <div class="input-field col s12">
+                    <i class="material-icons prefix">account_circle</i>
+                    <input type="text" name="name" id="name" class="validate">
+                    <label for="name">用户名:</label>
+                </div>
+            </div>
+            <div class="row">
+                <div class="input-field col s12">
+                    <i class="material-icons prefix">lock</i>
+                    <input type="password" name="passwd" id="passwd" class="validate">
+                    <label for="passwd">密码:</label>
+                </div>
+            </div>
+            <div class="row">
+                <button type="submit" class="btn">登录</button>
+                <a href="reg.php" class="btn">注册</a>
+            </div>
+        </form>
+    </div>
+
+    <script src="js/materialize.min.js"></script>
 </body>
+
 </html>
